23f42b2c95d4d70b9ac99ea7eec76b4cb7753beb
David Eliason <davethemaker@gmail.com>
2025-01-08 21:31:37 -0800

    add updated README and also the provider information and auth for AWS services and tthe VPC within us-west-2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terraform: Building Infrastructure with a Single EC2
+## 1/8/2025
+### [David Eliason](https://www.deliason.com)
+
+**description**
+I'm building this one step at a time, to understand using modules and passing variables
---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# configure the aws provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscodeaws"
+  region                   = "us-west-2"
+}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
