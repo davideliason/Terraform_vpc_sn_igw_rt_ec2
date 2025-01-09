b3d3238901c9f603840291b88843f15e27fa00cb
David Eliason <davethemaker@gmail.com>
2025-01-08 22:02:18 -0800

    removed tightly coupled vpc resource definition orignally used

--- main.tf
+++ main.tf
@@ -15,16 +15,6 @@ provider "aws" {
   region                   = "us-west-2"
 }
 
-#resource "aws_vpc" "my-vpc" {
-#  cidr_block           = "10.0.0.0/16"
-#  enable_dns_hostnames = true
-#  enable_dns_support   = true
-
-#  tags = {
-#    Name = "my-"
-#  }
-#}
-
 module "vpc" {
   source     = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
