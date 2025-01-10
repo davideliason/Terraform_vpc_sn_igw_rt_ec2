7d84c3b5ddf7e38af448edb4fb7f7d39a43a108c
David Eliason <davethemaker@gmail.com>
2025-01-10 04:11:09 -0800

    add public subnet cidr block instantiation to main file

--- main.tf
+++ main.tf
@@ -16,8 +16,9 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  source                   = "./modules/vpc"
+  cidr_block               = "10.0.0.0/16"
+  public_subnet_cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "my-vpc"
