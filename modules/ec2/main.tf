27628df042deab11e6960b4135a82c988b76d188
David Eliason <davethemaker@gmail.com>
2025-01-10 04:29:14 -0800

    add instance def as well as instance_id and public_ip outputs in ec2 module main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "my-public-server" {
+  ami           = var.ami
+  instance_type = var_instance_type
+  subnet_id     = module.vpc.public_subnet_id
+
+  tags = {
+    Name = "my-public-server"
+  }
+
+}
+
+output "instance_id" {
+  value = aws_instance.my-public-server.id
+
+}
+
+output "public_ip" {
+  value = aws_instance.my-public-server.public_ip
+
+}
